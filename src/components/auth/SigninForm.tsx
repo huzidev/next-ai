15b31035f1a7408c92aebb2c9f0c6020bbc75451
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-07-06 16:41:19 +0500

    boolean variable

--- src/components/auth/SigninForm.tsx
+++ src/components/auth/SigninForm.tsx
@@ -28,7 +28,7 @@ export default function SigninForm({
   onTogglePassword,
   variant
 }: SigninFormProps) {
-  const isAdmin = variant === 'admin';
+  const isAdmin: boolean = variant === 'admin';
   
   const colorClasses = isAdmin ? {
     focusColor: "focus:border-purple-400 focus:ring-purple-400",
