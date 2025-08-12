000e03c018c47489d621140020c4d89ca7fc0e0d
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-08-12 23:47:31 +0500

    on map users

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -42,7 +42,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     });
 
     // Format the response
-    const formattedUsers = users.map({ id } => ({
+    const formattedUsers = users.map(user => ({
       id: user.id,
       username: user.username,
       email: user.email,
